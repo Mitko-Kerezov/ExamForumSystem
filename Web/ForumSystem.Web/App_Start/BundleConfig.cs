405fafc7725975d0a4a6650b846001524e45b89e
Mitko-Kerezov <d.kerezov@gmail.com>
2014-11-17 15:46:33 +0200

    Kendo Fix

--- Web/ForumSystem.Web/App_Start/BundleConfig.cs
+++ Web/ForumSystem.Web/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
                 "~/Content/kendo/kendo.common.min.css",
                 "~/Content/kendo/kendo.common-bootstrap.min.css",
                 "~/Content/kendo/kendo.silver.min.css"));
